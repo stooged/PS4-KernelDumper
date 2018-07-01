190fef0aa1321e0d3ce0ae8a324c4f949d376332
stooged <stooged@users.noreply.github.com>
2018-07-01 12:52:34 +0800

    v1

--- build.sh
+++ build.sh
@@ -11,6 +11,13 @@ rm -f bin/KernelDumper.bin
 cp KernelDumper/KernelDumper.bin bin/KernelDumper.bin
 mkdir -p html_payload
 tool/bin2js bin/KernelDumper.bin > html_payload/payload.js
-sed "s/###/$(cat html_payload/payload.js)/" exploit.template > html_payload/KernelDumper.html
+FILESIZE=$(stat -c%s "bin/KernelDumper.bin")
+PNAME=$"Kernel Dumper"
+cp exploit.template html_payload/KernelDumper.html
+sed -i -f - html_payload/KernelDumper.html << EOF
+s/#NAME#/$PNAME/g
+s/#BUF#/$FILESIZE/g
+s/#PAY#/$(cat html_payload/payload.js)/g
+EOF
 rm -f KernelDumper/KernelDumper.bin
 rm -f html_payload/payload.js
